34b5959f3cf3fd6c83929591744c599828e977da
Chris Lemler <clemler@pm.me>
2019-10-21 20:15:11 -0700

    Initial non-concurrent versin of clock server

---
+++ concurrency/clock/clock.go
@@ -0,0 +1,40 @@
+// clock - simple TCP server that published time once a second
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	fmt.Println("clock starting on port 8000")
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn)  // Handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
